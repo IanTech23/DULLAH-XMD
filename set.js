448e171768f35a88e08661cc800550a8aca3195d
IanTech23 <ianheroku4@gmail.com>
2025-06-07 07:45:34 +0300

    Update set.js

--- set.js
+++ set.js
@@ -8,7 +8,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
-    PREFIXE: process.env.PREFIX || "*",
+    PREFIXE: process.env.PREFIX || "+",
     OWNER_NAME: process.env.OWNER_NAME || "IAN TECH v²",
     NUMERO_OWNER : process.env.NUMERO_OWNER || "254723278526",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
